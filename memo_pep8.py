2a15307b152c7bd6d8496642deaed94eff13e167
JosephYu <josephyu.careers@outlook.com>
2021-01-14 08:31:34 +0800

    Update memo

---
+++ memo_pep8.py
@@ -0,0 +1,26 @@
+# https://realpython.com/python-pep8/#why-we-need-pep-8
+
+"""
+
+# underscore is always used here!
+
+function, my_function
+
+x, var, my_variable
+
+Class: start each word with a capital letter - Employee, TestCalc, 
+
+method: class_method, method, test_add(self), test_divide(self)
+
+constant: CONSTANT, MY_CONSTANT, MY_LONG_CONSTANT
+
+module: module.py, my_module.py
+
+package: package, mypackage
+
+        package
+
+            mod1.py
+            mod2.py
+
+"""
---
+++ test_jyu_3.py
@@ -0,0 +1,14 @@
+import unittest
+from employee import Employee
+
+
+class TestEmployee(unittest.TestCase):
+
+    # 👀 These are camel-cased with the Upper case on the 1st letter of the second word
+    # setUp, tearDown - which is NOT what Python normally do?
+    # what's the Pythonic way of writing this?
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
