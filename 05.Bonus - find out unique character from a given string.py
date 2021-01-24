442119ce41685e1c53f84ca5fe2814339cdcebf8
JosephYu <josephyu.careers@outlook.com>
2021-01-24 17:05:28 +0800

    create 05 Bonus

---
+++ 05.Bonus - find out unique character from a given string.py
@@ -0,0 +1,50 @@
+# find out unique character (E.g. numbers, digis, signs) from a given text
+
+
+import re
+
+text_to_search = '''
+abcdefghijklmnopqurtuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+1234567890
+
+Ha HaHa
+
+MetaCharacters (Need to be escaped):
+. ^ $ * + ? { } [ ] \ | ( )
+
+coreyms.com
+
+321-555-4321
+123.555.1234
+123*555*1234
+800-555-1234
+900-555-1234
+
+Mr. Schafer
+Mr Smith
+Ms Davis
+Mrs. Robinson
+Mr. T
+'''
+
+sentence = 'Start a sentence and then bring it to an end'
+
+pattern = re.compile(r'.')
+
+# Match pattern
+matches = pattern.finditer(text_to_search)
+
+li_items = []
+
+for match in matches:
+
+    item = str(match)[-3:-2]
+
+    li_items.append(item)
+
+print(li_items)
+print(len(li_items))
+
+print(set(li_items))
+print(len(set(li_items)))
---
+++ 05.re Module - How to Write and Match Regular Expressions (Regex).py
@@ -0,0 +1,62 @@
+import re
+
+text_to_search = '''
+abcdefghijklmnopqurtuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+1234567890
+
+Ha HaHa
+
+MetaCharacters (Need to be escaped):
+. ^ $ * + ? { } [ ] \ | ( )
+
+coreyms.com
+
+321-555-4321
+123.555.1234
+123*555*1234
+800-555-1234
+900-555-1234
+
+Mr. Schafer
+Mr Smith
+Ms Davis
+Mrs. Robinson
+Mr. T
+'''
+
+sentence = 'Start a sentence and then bring it to an end'
+
+# Backslash \t -> tab
+# 🧠 '\t'
+
+print('\tTab')
+
+# r -> raw string
+# 🧠 r'string'
+print(r'\tTab')
+
+# Create pattern: raw string mast to be EXACT match
+pattern = re.compile(r'.')
+
+# Match pattern
+matches = pattern.finditer(text_to_search)
+
+for match in matches:
+    print(match)
+
+print(text_to_search[1:4])
+print(text_to_search[28:31])
+
+
+"""
+# 🎯 https://stackoverflow.com/a/1548720/15063197
+
+import os, re
+
+def purge(dir, pattern):
+    for f in os.listdir(dir):
+        if re.search(pattern, f):
+            os.remove(os.path.join(dir, f))
+
+"""
